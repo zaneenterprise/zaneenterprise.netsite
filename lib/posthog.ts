2d2dc197fa08bbd86d5052704474f4550edf701f
Christian Cash <christian@zaneenterprise.net>
2025-10-30 08:40:02 -0700

    posthog update

--- lib/posthog.ts
+++ lib/posthog.ts
@@ -1,6 +1,5 @@
 import { PostHog } from "posthog-node";
 
-// NOTE: This is a Node.js client, so you can use it for sending events from the server side to PostHog.
 export default function PostHogClient() {
   const posthogClient = new PostHog(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
     host: process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com",

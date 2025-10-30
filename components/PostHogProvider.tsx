422a1c412ff213b3c50f5865104c6591bf2152a9
Christian Cash <christian@zaneenterprise.net>
2025-10-30 08:10:12 -0700

    update posthog

--- components/PostHogProvider.tsx
+++ components/PostHogProvider.tsx
@@ -6,13 +6,18 @@ import { useEffect } from "react"
 
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
-    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-      api_host: "/ingest",
-      ui_host: "https://us.posthog.com",
-      defaults: '2025-05-24',
-      capture_exceptions: true, // This enables capturing exceptions using Error Tracking, set to false if you don't want this
-      debug: process.env.NODE_ENV === "development",
-    })
+    if (typeof window !== 'undefined') {
+      posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+        api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com",
+        ui_host: "https://us.posthog.com",
+        person_profiles: 'identified_only',
+        capture_pageview: false, // Disabled because we use automatic tracking with defaults
+        capture_pageleave: true,
+        autocapture: true,
+        capture_exceptions: true,
+        debug: process.env.NODE_ENV === "development",
+      })
+    }
   }, [])
 
   return (
--- lib/posthog.ts
+++ lib/posthog.ts
@@ -3,7 +3,7 @@ import { PostHog } from "posthog-node";
 // NOTE: This is a Node.js client, so you can use it for sending events from the server side to PostHog.
 export default function PostHogClient() {
   const posthogClient = new PostHog(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-    host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    host: process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com",
     flushAt: 1,
     flushInterval: 0,
   });
